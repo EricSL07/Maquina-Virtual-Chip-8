bcaedd50cd55496bfd2f8b801348c814b2710a0f
EricSL07 <ericseles@alunos.utfpr.edu.br>
2025-09-28 22:30:48 -0300

    Primeira ideia de como vai ser o projeto

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+struct Chip8 {
+    uint8_t memory[4096];
+    uint8_t V[16];
+    uint16_t I;
+    uint16_t pc;
+    uint8_t gfx[64 * 32];
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+    uint16_t stack[16];
+    uint8_t sp;
+    uint8_t key[16];
+} chip8_t; 
+
