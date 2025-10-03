832c6e8173ba0b78571a2d3346224a2dc0dd6606
EricSL07 <ericseles@alunos.utfpr.edu.br>
2025-10-02 22:50:15 -0300

    Implement Chip-8 virtual machine with ROM loading and instruction execution

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
-#include <stdint.h>
+#include "chip8.h"
+#include "defs.h"
 
-struct Chip8 {
-    uint8_t memory[4096];
-    uint8_t V[16];
-    uint16_t I;
-    uint16_t pc;
-    uint8_t gfx[64 * 32];
-    uint8_t delay_timer;
-    uint8_t sound_timer;
-    uint16_t stack[16];
-    uint8_t sp;
-    uint8_t key[16];
-} chip8_t; 
+int main(int argc, char **argv)
+{
+    VM vm;
+    VM_Inicializar(&vm, 0x200); // Inicializa a VM (PC = 0x200)
 
+    VM_CarregarROM(&vm, argv[1], 0x200); // Carrega a ROM de um arquivo passado como argumento pela linha de comando
+#ifdef DEBUG
+    VM_ImprimirRegistradores(&vm);
+#endif
+    while (1)
+    {
+        VM_ExecutarInstrucao(&vm);
+// Executa uma instrução
+#ifdef DEBUG
+        VM_ImprimirRegistradores(&vm);
+#endif
+    }
+}
